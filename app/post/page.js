95d67588405ccb022a4e7c6014c33f085ab8f38b
Roniebin <killpig00@naver.com>
2024-03-05 11:36:34 +0900

    ff

--- app/post/page.js
+++ app/post/page.js
@@ -2,26 +2,23 @@ import { connectDB } from "@/util/database";
 import Posting from "./Posting";
 import Link from "next/link";
 
-export default async function post()
-{
+export default async function post() {
 
-    let client = await connectDB; 
-    const db = client.db('blog'); 
+    let client = await connectDB;
+    const db = client.db('blog');
     let result = await db.collection('post').find().toArray();
-   
-    return(
+
+    return (
         <div className="post-container maximum-width">
             <div className="post-title">
+                <h3>자유 게시판</h3><Link href="/post/postwrite" className="post-writing">글쓰기</Link>
+            </div>
 
-          
-           <h3>자유 게시판</h3><Link href="/post/postwrite" className="post-writing">글쓰기</Link>
-           </div> 
+            <Posting result={result} />
 
-          <Posting result={result} />
 
-          
-           
-        
+
+
         </div>
     )
 }
--- pages/api/post/postdelete.js
+++ pages/api/post/postdelete.js
@@ -3,32 +3,29 @@ import { authOptions } from "../auth/[...nextauth]";
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 
-export default async function handler(요청,응답)
-{
-  
+export default async function handler(요청, 응답) {
+
     // 자기자신 게시글이나 관리자만 삭제할수있게 확인용
-    let session= await getServerSession(요청,응답,authOptions)
+    let session = await getServerSession(요청, 응답, authOptions)
+    let a;
     
-    if(session)
-    {
-        if (요청.method=="DELETE")
-        {
+    if (session) {
+        if (요청.method == "DELETE") {
             const client = await connectDB;
-            const db=client.db("blog");
-            
+            const db = client.db("blog");
+
             console.log(session.user)
-            let post=await db.collection("post").findOne({_id: new ObjectId(요청.body)})
-        
-            if(post.author==session.user.email || session.user.email=="admin@naver.com")
-            {
-                await db.collection("post").deleteOne({_id:new ObjectId(요청.body)})
+            let post = await db.collection("post").findOne({ _id: new ObjectId(요청.body) })
+
+            if (post.author == session.user.email || session.user.email == "admin@naver.com") {
+                await db.collection("post").deleteOne({ _id: new ObjectId(요청.body) })
                 응답.status(200).redirect("/post")
             }
-        }else{
+        } else {
             응답.status(200).json("삭제 권한이 없습니다.")
         }
-    }else{
+    } else {
         응답.status(200).json("삭제 권한이 없습니다.")
     }
-   
+
 }
